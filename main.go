b5234d48ca1b3ad4bd0ac2160de37c58ac03d1ad
apstndb <803393+apstndb@users.noreply.github.com>
2024-11-04 19:06:35 +0900

    Add --use-embedded-emulator and --emulator-image

--- main.go
+++ main.go
@@ -60,6 +60,13 @@ type spannerOptions struct {
 	ProtoDescriptorFile string            `long:"proto-descriptor-file" description:"Path of a file that contains a protobuf-serialized google.protobuf.FileDescriptorSet message."`
 	Insecure            bool              `long:"insecure" description:"Skip TLS verification and permit plaintext gRPC. --skip-tls-verify is an alias."`
 	SkipTlsVerify       bool              `long:"skip-tls-verify" description:"An alias of --insecure" hidden:"true"`
+	UseEmbeddedEmulator bool              `long:"use-embedded-emulator" description:"Use embedded Cloud Spanner Emulator. Implies --endpoint and --insecure."`
+	EmulatorImage       string            `long:"emulator-image" description:"container image for --use-embedded-emulator"`
+	Help                bool              `long:"help" short:"h" hidden:"true"`
+}
+
+func addEmulatorImageOption(parser *flags.Parser) {
+	parser.Groups()[0].Find("spanner").FindOptionByLongName("emulator-image").DefaultMask = defaultEmulatorImage
 }
 
 const (
@@ -71,20 +78,34 @@ var logMemefish bool
 
 func main() {
 	var gopts globalOptions
+
 	// process config files at first
-	if err := readConfigFile(flags.NewParser(&gopts, flags.Default)); err != nil {
+	configFileParser := flags.NewParser(&gopts, flags.Default)
+	addEmulatorImageOption(configFileParser)
+
+	if err := readConfigFile(configFileParser); err != nil {
 		exitf("Invalid config file format\n")
 	}
 
 	// then, process environment variables and command line options
 	// use another parser to process environment variables with higher precedence than configuration files
-	flagParser := flags.NewParser(&gopts, flags.Default)
+	// flagParser := flags.NewParser(&gopts, flags.PrintErrors|flags.PassDoubleDash)
+	flagParser := flags.NewParser(&gopts, flags.PrintErrors|flags.PassDoubleDash)
+	addEmulatorImageOption(flagParser)
+
+	// TODO: Workaround to avoid to display config value as default
+	parserForHelp := flags.NewParser(&globalOptions{}, flags.Default)
+	addEmulatorImageOption(parserForHelp)
+
 	if _, err := flagParser.Parse(); flags.WroteHelp(err) {
 		// exit successfully
 		return
 	} else if err != nil {
-		flags.NewParser(&gopts, flags.Default).WriteHelp(os.Stderr)
+		parserForHelp.WriteHelp(os.Stderr)
 		exitf("Invalid options\n")
+	} else if gopts.Spanner.Help {
+		parserForHelp.WriteHelp(os.Stderr)
+		return
 	}
 
 	opts := gopts.Spanner
@@ -159,6 +180,21 @@ func main() {
 	}
 
 	ctx := context.Background()
+
+	if opts.UseEmbeddedEmulator {
+		container, teardown, err := newEmulator(ctx, opts)
+		if err != nil {
+			exitf("failed to start Cloud Spanner Emulator: %v\n", err)
+		}
+		defer teardown()
+
+		sysVars.Endpoint = container.URI
+		sysVars.Insecure = true
+
+		if err := setUpEmptyInstanceAndDatabaseForEmulator(ctx, &sysVars); err != nil {
+			exitf("failed to setup instance and database in emulator: %v\n", err)
+		}
+	}
 
 	cli, err := NewCli(ctx, cred, os.Stdin, os.Stdout, os.Stderr, &sysVars)
 	if err != nil {
