1585341cf8a3f7c7a64e3f00439657c00b01cfff
apstndb <803393+apstndb@users.noreply.github.com>
2024-11-30 14:40:29 +0900

    Implement SHOW QUERY PROFILE

--- statement.go
+++ statement.go
@@ -169,6 +169,7 @@ var (
 	tryPartitionedQueryRe = regexp.MustCompile(`(?is)^TRY\s+PARTITIONED\s+QUERY\s(\S.*)$`)
 	mutateRe              = regexp.MustCompile(`(?is)MUTATE\s+(\S+)\s+(INSERT|UPDATE|INSERT_OR_UPDATE|REPLACE|DELETE)\s+(.+)$`)
 	showQueryProfilesRe   = regexp.MustCompile(`(?is)^SHOW\s+QUERY\s+PROFILES$`)
+	showQueryProfileRe    = regexp.MustCompile(`(?is)^SHOW\s+QUERY\s+PROFILE\s+(.*)$`)
 )
 
 var (
@@ -294,6 +295,13 @@ func BuildCLIStatement(trimmed string) (Statement, error) {
 		return &MutateStatement{Table: unquoteIdentifier(matched[1]), Operation: matched[2], Body: matched[3]}, nil
 	case showQueryProfilesRe.MatchString(trimmed):
 		return &ShowQueryProfilesStatement{}, nil
+	case showQueryProfileRe.MatchString(trimmed):
+		matched := showQueryProfileRe.FindStringSubmatch(trimmed)
+		fprint, err := strconv.ParseInt(strings.TrimSpace(matched[1]), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		return &ShowQueryProfileStatement{Fprint: fprint}, nil
 	default:
 		return nil, errStatementNotMatched
 	}
@@ -1257,6 +1265,29 @@ type queryProfilesRow struct {
 	QueryProfile    *queryProfiles   `spanner:"-"`
 }
 
+func toQpr(row *spanner.Row) (*queryProfilesRow, error) {
+	var qpr queryProfilesRow
+	if err := row.ToStruct(&qpr); err != nil {
+		return nil, err
+	}
+
+	var profile queryProfiles
+	err := json.Unmarshal([]byte(qpr.RawQueryProfile.String()), &profile)
+	if err != nil {
+		return nil, err
+	}
+	qpr.QueryProfile = &profile
+
+	var queryPlan sppb.QueryPlan
+	err = protojson.Unmarshal(profile.RawQueryPlan, &queryPlan)
+	if err != nil {
+		return nil, err
+	}
+	profile.QueryPlan = &queryPlan
+
+	return &qpr, nil
+}
+
 func (s *ShowQueryProfilesStatement) Execute(ctx context.Context, session *Session) (*Result, error) {
 	if session.InReadWriteTransaction() {
 		// INFORMATION_SCHEMA can't be used in read-write transaction.
@@ -1270,34 +1301,14 @@ func (s *ShowQueryProfilesStatement) Execute(ctx context.Context, session *Sessi
 
 	iter, _ := session.RunQuery(ctx, stmt)
 
-	rows, _, _, _, _, err := consumeRowIterCollect(iter, func(row *spanner.Row) (*queryProfilesRow, error) {
-		var qpr queryProfilesRow
-		if err := row.ToStruct(&qpr); err != nil {
-			return nil, err
-		}
-		return &qpr, nil
-	})
+	rows, _, _, _, _, err := consumeRowIterCollect(iter, toQpr)
 	if err != nil {
 		return nil, err
 	}
 
 	var resultRows []Row
 	for _, row := range rows {
-		var profile queryProfiles
-		err := json.Unmarshal([]byte(row.RawQueryProfile.String()), &profile)
-		if err != nil {
-			return nil, err
-		}
-		row.QueryProfile = &profile
-
-		var queryPlan sppb.QueryPlan
-		err = protojson.Unmarshal(profile.RawQueryPlan, &queryPlan)
-		if err != nil {
-			return nil, err
-		}
-		profile.QueryPlan = &queryPlan
-
-		rows, predicates, err := processPlanWithStats(&queryPlan)
+		rows, predicates, err := processPlanWithStats(row.QueryProfile.QueryPlan)
 		if err != nil {
 			return nil, err
 		}
@@ -1313,7 +1324,7 @@ func (s *ShowQueryProfilesStatement) Execute(ctx context.Context, session *Sessi
 			},
 			slices.Values(rows))), "\n")
 
-		resultRows = append(resultRows, toRow(runewidth.FillRight("ID", maxIDLength)+" | Plan\n"+tree+
+		resultRows = append(resultRows, toRow(row.QueryProfile.QueryStats.QueryText+"\n"+runewidth.FillRight("ID", maxIDLength)+" | Plan\n"+tree+
 			lox.IfOrEmpty(len(predicates) > 0, "\nPredicates:\n"+strings.Join(predicates, "\n"))+"\n"+
 			formatStats(row)))
 	}
@@ -1354,6 +1365,56 @@ func formatStats(stats *queryProfilesRow) string {
 	}
 
 	return sb.String()
+}
+
+type ShowQueryProfileStatement struct {
+	Fprint int64
+}
+
+func (s *ShowQueryProfileStatement) Execute(ctx context.Context, session *Session) (*Result, error) {
+	if session.InReadWriteTransaction() {
+		// INFORMATION_SCHEMA can't be used in read-write transaction.
+		// https://cloud.google.com/spanner/docs/information-schema
+		return nil, fmt.Errorf(`%q can not be used in a read-write transaction`, `SPANNER_SYS.QUERY_PROFILES_TOP_HOUR`)
+	}
+
+	stmt := spanner.Statement{
+		SQL: `SELECT INTERVAL_END, TEXT_FINGERPRINT, LATENCY_SECONDS, PARSE_JSON(QUERY_PROFILE) AS QUERY_PROFILE
+FROM SPANNER_SYS.QUERY_PROFILES_TOP_HOUR
+WHERE TEXT_FINGERPRINT = @fprint
+ORDER BY INTERVAL_END DESC`,
+		Params: map[string]interface{}{"fprint": s.Fprint},
+	}
+
+	iter, _ := session.RunQuery(ctx, stmt)
+
+	qprs, _, _, _, _, err := consumeRowIterCollect(iter, toQpr)
+	if err != nil {
+		return nil, err
+	}
+
+	qpr, ok := lo.First(qprs)
+	if !ok {
+		return nil, errors.New("empty result")
+	}
+
+	rows, predicates, err := processPlanWithStats(qpr.QueryProfile.QueryPlan)
+	if err != nil {
+		return nil, err
+	}
+
+	// ReadOnlyTransaction.Timestamp() is invalid until read.
+	result := &Result{
+		ColumnNames:  explainAnalyzeColumnNames,
+		ColumnAlign:  explainAnalyzeColumnAlign,
+		ForceVerbose: true,
+		AffectedRows: len(rows),
+		Stats:        qpr.QueryProfile.QueryStats,
+		Rows:         rows,
+		Predicates:   predicates,
+		LintResults:  lox.IfOrEmptyF(session.systemVariables.LintPlan, func() []string { return lintPlan(qpr.QueryProfile.QueryPlan) }),
+	}
+	return result, nil
 }
 
 type NopStatement struct{}
