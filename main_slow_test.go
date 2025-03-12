3c4bc9ed4ce66ef537a53e5a4b7768fb158b1391
apstndb <803393+apstndb@users.noreply.github.com>
2025-03-12 19:16:27 +0900

    Add TestRun

---
+++ main_slow_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"context"
+	"testing"
+)
+
+// TestRun ensure that --embedded-emulator doesn't need ADC.
+func TestRun(t *testing.T) {
+	exitCode := run(context.Background(), &spannerOptions{Execute: "SELECT 1", EmbeddedEmulator: true})
+	if exitCode != exitCodeSuccess {
+		t.Errorf("exitCode != exitCodeSuccess, exitCode: %v", exitCode)
+	}
+}
