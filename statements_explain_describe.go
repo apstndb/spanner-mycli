c400507d203f243bbdf4d87b1c840803923aad06
apstndb <803393+apstndb@users.noreply.github.com>
2025-06-06 01:25:20 +0900

    Fix error handling

--- statements_explain_describe.go
+++ statements_explain_describe.go
@@ -77,11 +77,11 @@ type ExplainLastQueryStatement struct {
 }
 
 func (s *ExplainLastQueryStatement) Execute(ctx context.Context, session *Session) (*Result, error) {
-	if len(session.systemVariables.LastQueryCache.QueryPlan.GetPlanNodes()) == 0 {
+	if session.systemVariables.LastQueryCache == nil {
 		return nil, fmt.Errorf("last query cache missing. Have you run a query")
 	}
 
-	if session.systemVariables.LastQueryCache == nil || len(session.systemVariables.LastQueryCache.QueryPlan.GetPlanNodes()) == 0 {
+	if session.systemVariables.LastQueryCache.QueryPlan != nil || len(session.systemVariables.LastQueryCache.QueryPlan.GetPlanNodes()) == 0 {
 		return nil, fmt.Errorf("missing last query plan. If you use cloud-spanner-emulator, it doesn't support EXPLAIN and EXPLAIN ANALYZE")
 	}
 
